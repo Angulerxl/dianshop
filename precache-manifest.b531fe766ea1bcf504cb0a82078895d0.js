a8116c072f097c78a7907c46ebfc9ab10acb985a
Angulerxl <670448045@qq.com>
2025-04-13 01:54:42 +0800

    Updates

---
+++ precache-manifest.b531fe766ea1bcf504cb0a82078895d0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8af1f1bb0c307c9eb6",
+    "url": "css/app.16d176e5.css"
+  },
+  {
+    "revision": "8da868996e500f3b8f6c",
+    "url": "css/chunk-vendors.6b92dc4c.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "e662dbc757afd2463c40a221c77268bb",
+    "url": "img/image0.e662dbc7.png"
+  },
+  {
+    "revision": "91a8d457b709ed7fd23692ce35ad2b81",
+    "url": "img/image1-0.91a8d457.png"
+  },
+  {
+    "revision": "aa462bd5d8c2c94c4ed4e134363e7f28",
+    "url": "img/image1-1.aa462bd5.png"
+  },
+  {
+    "revision": "933b8e0e01f6eaa4cb17a33436fd1ae8",
+    "url": "img/image1-2.933b8e0e.png"
+  },
+  {
+    "revision": "f3a03e949551e40ffd103c8b880b8708",
+    "url": "img/image2.f3a03e94.png"
+  },
+  {
+    "revision": "8f1bf38a40e5f13862795af61c4a7e35",
+    "url": "img/image3.8f1bf38a.png"
+  },
+  {
+    "revision": "fac3c4e6598c8010a8ac4339d337c336",
+    "url": "img/image4.fac3c4e6.png"
+  },
+  {
+    "revision": "321c378b04042c92b4019130694f2cd8",
+    "url": "img/image5.321c378b.png"
+  },
+  {
+    "revision": "cee0cb7908de68698650985213d379cb",
+    "url": "img/image6.cee0cb79.png"
+  },
+  {
+    "revision": "7f3f4756ae3684e99b83f25c06279c2b",
+    "url": "index.html"
+  },
+  {
+    "revision": "3c8af1f1bb0c307c9eb6",
+    "url": "js/app.9f721b26.js"
+  },
+  {
+    "revision": "8da868996e500f3b8f6c",
+    "url": "js/chunk-vendors.6e62e830.js"
+  },
+  {
+    "revision": "c40041a561ab8218f55458da96ecf79d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "5bf1e1596d26e2d9a997018e6a072457",
+    "url": "table.html"
+  }
+]);
