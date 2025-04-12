3b04500ad760275d08e069e5fc3418b9ca4ae7a3
Angulerxl <670448045@qq.com>
2025-04-13 01:07:42 +0800

    初始化

--- dist/precache-manifest.b89a1e5e7d81c7ac106a441360c166e1.js
+++ dist/precache-manifest.b89a1e5e7d81c7ac106a441360c166e1.js
@@ -1,42 +1,42 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eea49d3d9460f11282b5",
-    "url": "/css/app.2a98477a.css"
+    "revision": "7f5f05579468df3330a8",
+    "url": "css/app.2a98477a.css"
   },
   {
     "revision": "8da868996e500f3b8f6c",
-    "url": "/css/chunk-vendors.6b92dc4c.css"
+    "url": "css/chunk-vendors.6b92dc4c.css"
   },
   {
     "revision": "535877f50039c0cb49a6196a5b7517cd",
-    "url": "/fonts/element-icons.535877f5.woff"
+    "url": "fonts/element-icons.535877f5.woff"
   },
   {
     "revision": "732389ded34cb9c52dd88271f1345af9",
-    "url": "/fonts/element-icons.732389de.ttf"
+    "url": "fonts/element-icons.732389de.ttf"
   },
   {
-    "revision": "ee0ce90a3c2650ba288ff99af0f620af",
-    "url": "/index.html"
+    "revision": "584dd7f6a186a90e85e9f2680758fa6a",
+    "url": "index.html"
   },
   {
-    "revision": "eea49d3d9460f11282b5",
-    "url": "/js/app.31867f66.js"
+    "revision": "7f5f05579468df3330a8",
+    "url": "js/app.a9cce5a3.js"
   },
   {
     "revision": "8da868996e500f3b8f6c",
-    "url": "/js/chunk-vendors.6e62e830.js"
+    "url": "js/chunk-vendors.6e62e830.js"
   },
   {
     "revision": "c40041a561ab8218f55458da96ecf79d",
-    "url": "/manifest.json"
+    "url": "manifest.json"
   },
   {
     "revision": "735ab4f94fbcd57074377afca324c813",
-    "url": "/robots.txt"
+    "url": "robots.txt"
   },
   {
     "revision": "5bf1e1596d26e2d9a997018e6a072457",
-    "url": "/table.html"
+    "url": "table.html"
   }
 ]);
